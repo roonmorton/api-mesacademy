7801a7a836b90303f443ed93bfae226d8a756a31
Ron <roonmorton@gmail.com>
2019-07-30 13:59:19 -0600

    my-response
    updated code error 400

--- src/config/my-response.js
+++ src/config/my-response.js
@@ -1,6 +1,7 @@
 module.exports = {
     send: (res, result, myinfo, message, err = null) => {
         if (err) {
+            res.status(400);
             res.send({
                 info: err.message,
                 message: message || '',
